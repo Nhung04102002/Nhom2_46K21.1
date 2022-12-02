49a8d069f1af628ffb0b902cdd008a7c2f0bfc9a
Doan Nhung <doanthaonhung@outlook.com>
2022-12-02 18:00:07 +0700

    update1

--- QLCH_Nhom2/Pages/Index.cshtml.cs
+++ QLCH_Nhom2/Pages/Index.cshtml.cs
@@ -1,20 +1,81 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using QLCH_Nhom2.Pages.Hang;
+using System.Data.SqlClient;
 
 namespace QLCH_Nhom2.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
+        public TKInfo loginInfo = new TKInfo();
+        public String errorMessage = "";
+        public String successMessage = "";
         public void OnGet()
         {
 
         }
+        public void OnPost()
+        {
+            loginInfo.UserName = Request.Form["UserName"];
+            loginInfo.Pass = Request.Form["Pass"];
+
+            if (loginInfo.UserName.Length == 0 || loginInfo.Pass.Length == 0)
+            {
+                errorMessage = "Vui lòng nhập username và password";
+                return;
+            }
+
+            try
+            {
+                String connectionString = "Data Source=.\\sqlexpress;Initial Catalog=Nhom2_QLBH;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    String sql1 = "select dbo.fCheckLogin (@UserName,@Pass)";
+
+                    using (SqlCommand command = new SqlCommand(sql1, connection))
+                    {
+                        command.Parameters.AddWithValue("@UserName", loginInfo.UserName);
+                        command.Parameters.AddWithValue("@Pass", loginInfo.Pass);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                loginInfo.result = reader.GetInt32(0);
+                            }
+
+                            
+                        }
+                        if (loginInfo.result == 1)
+                        {
+                            successMessage = "Đăng nhập thành công!";
+                            Response.Redirect("/TrangChu/Index");
+                        }
+                        else
+                        {
+                            errorMessage = "Username hoặc password không đúng";
+                            return;
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+                return;
+            }
+
+        }
+    }
+    public class TKInfo
+    {
+        public string? UserName;
+        public string? Pass;
+        public int result;
     }
 }
---
+++ QLCH_Nhom2/Pages/TrangChu/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QLCH_Nhom2.Pages.TrangChu
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
